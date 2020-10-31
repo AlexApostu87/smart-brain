6f1b59c340bff2d37919c77c9573b4b78f3cac8b
AlexApostu87 <apostu.alex1987@gmail.com>
2020-10-31 17:45:52 +0200

    comitting

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('https://smart-brain987.herokuapp.com/signin', {
+		fetch('https://creepy-ghost-19568.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
